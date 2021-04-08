9c9886b1da8f75dfc55d5705786f4bfadcb500bd
Hasna Rifky Afifah <hasnarifkya@gmail.com>
2021-04-08 07:43:35 +0700

    Tugas 6

--- I0320047_Soal2.py
+++ I0320047_Soal2.py
@@ -1,10 +1,11 @@
 #program menghitung nilai rata-rata
 
+print("Selamat datang di program perhitungan rata-rata")
+print(" ")
 print("Masukkan data-data yang dibutuhkan")
 n = int(input("Banyaknya nilai yang akan dihitung : "))
 i = 1
 
-print()
 list_nilai =[]
 jumlah_nilai = 0
 
@@ -16,4 +17,4 @@ while i <= n :
 jumlah_nilai = sum(list_nilai)
 rata = jumlah_nilai/n
 print(" ")
-print("Nilai rata-rata yang diinput pengguna adalah", '%0.3f' % rata)
+print("Rata-rata dari nilai yang diinput pengguna adalah", '%0.3f' % rata)
